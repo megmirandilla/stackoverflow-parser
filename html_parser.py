742df0f18d16f45b5f4bab52ab48fddb60d9005a
Meg Mirandilla <mamirandilla@up.edu.ph>
2018-06-07 16:15:18 +0800

    Extracted the question

--- html_parser.py
+++ html_parser.py
@@ -5,5 +5,19 @@ response = urllib.request.urlopen('https://stackoverflow.com/questions/50729310/
 html = response.read()
 
 soup = BeautifulSoup(html, 'html.parser')
-print(soup.get_text())
+# print(soup.get_text())
 
+head_tag = soup.head
+link_tag = soup.link
+
+# print(head_tag.contents)
+
+question_title = soup.find("a",{"class":"question-hyperlink"}).text
+question_body = soup.find("div",{"class":"question"}).find("p").text
+question = [question_title,question_body]
+print(question)
+
+
+
+# for obj in question:
+# 	print(obj)
