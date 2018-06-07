50ea6af6f46e046564646a1ae79edb7b39790659
Meg Mirandilla <mamirandilla@up.edu.ph>
2018-06-07 14:07:18 +0800

    Initial use of BeautifulSoup

--- html_parser.py
+++ html_parser.py
@@ -1,5 +1,9 @@
 import urllib.request
-response = urllib.request.urlopen('https://stackoverflow.com/questions/7735866/how-to-create-soap-client')
+from bs4 import BeautifulSoup
+
+response = urllib.request.urlopen('https://stackoverflow.com/questions/50729310/trouble-fetching-a-link-from-a-dynamic-webpage')
 html = response.read()
 
-print(html)
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.get_text())
+
